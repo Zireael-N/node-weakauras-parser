Zireael-N/node-weakauras-parser

commit 6377c13e7a39872a7443cff073eb609efeeae47c

Migrate to weakauras-codec

native/src/common.rs
@@ -1,19 +1,5 @@
 use neon::prelude::*;
-
-use std::borrow::Cow;
-
-use super::base64;
-use super::huffman;
-
-use super::ace_serialize::{Deserializer as LegacyDeserializer, Serializer as LegacySerializer};
-use super::lib_serialize::{Deserializer, Serializer};
-
-#[derive(Clone, Copy, PartialEq, Eq)]
-pub enum StringVersion {
-    Huffman,             // base64
-    Deflate,             // '!' + base64
-    BinarySerialization, // !WA:\d+! + base64
-}
+use weakauras_codec::OutputStringVersion;
 
 pub fn parse_max_size<'a>(
     v: Option<Handle<'a, JsValue>>,
@@ -45,98 +31,22 @@ pub fn parse_max_size<'a>(
 pub fn parse_string_version<'a>(
     v: Option<Handle<'a, JsValue>>,
     cx: &'a mut FunctionContext,
-) -> NeonResult<StringVersion> {
+) -> NeonResult<OutputStringVersion> {
     match v {
         Some(v) => {
             if v.is_a::<JsUndefined, _>(cx) {
-                Ok(StringVersion::BinarySerialization)
+                Ok(OutputStringVersion::BinarySerialization)
             } else {
                 v.downcast_or_throw::<JsNumber, _>(cx).and_then(|v| {
                     let v = v.value(cx).trunc() as u64;
                     match v {
-                        1 => Ok(StringVersion::Deflate),
-                        2 => Ok(StringVersion::BinarySerialization),
+                        1 => Ok(OutputStringVersion::Deflate),
+                        2 => Ok(OutputStringVersion::BinarySerialization),
                         _ => cx.throw_type_error("Invalid value"),
                     }
                 })
             }
         }
-        None => Ok(StringVersion::BinarySerialization),
+        None => Ok(OutputStringVersion::BinarySerialization),
     }
-}
-
-pub fn decode_weakaura(src: &str, max_size: Option<usize>) -> Result<String, &'static str> {
-    let (weakaura, version) = if let Some(src) = src.strip_prefix("!WA:2!") {
-        (src, StringVersion::BinarySerialization)
-    } else if let Some(src) = src.strip_prefix('!') {
-        (src, StringVersion::Deflate)
-    } else {
-        (src, StringVersion::Huffman)
-    };
-
-    let decoded = base64::decode(weakaura.trim_ascii_end())?;
-
-    let max_size = max_size.unwrap_or(usize::MAX);
-    let decompressed = if version == StringVersion::Huffman {
-        huffman::decompress(&decoded, max_size)
-    } else {
-        use flate2::read::DeflateDecoder;
-        use std::io::prelude::*;
-
-        let mut result = Vec::new();
-        let mut inflater = DeflateDecoder::new(&decoded[..]).take(max_size as u64);
-
-        inflater
-            .read_to_end(&mut result)
-            .map_err(|_| "Decompression error")
-            .and_then(|_| {
-                if result.len() < max_size {
-                    Ok(())
-                } else {
-                    match inflater.into_inner().bytes().next() {
-                        Some(_) => Err("Compressed data is too large"),
-                        None => Ok(()),
-                    }
-                }
-            })
-            .map(|_| Cow::from(result))
-    }?;
-
-    let deserialized = if version == StringVersion::BinarySerialization {
-        Deserializer::from_slice(&decompressed).deserialize_first()
-    } else {
-        LegacyDeserializer::from_str(&String::from_utf8_lossy(&decompressed)).deserialize_first()
-    }?;
-
-    serde_json::to_string(&deserialized).map_err(|_| "Failed to convert to JSON")
-}
-
-pub fn encode_weakaura(json: &str, format: StringVersion) -> Result<String, &'static str> {
-    let data = serde_json::from_str(json).map_err(|_| "Failed to parse JSON")?;
-
-    let (serialized, prefix) = match format {
-        StringVersion::Deflate => (
-            LegacySerializer::serialize(&data, Some(json.len())).map(|v| v.into_bytes())?,
-            "!",
-        ),
-        StringVersion::BinarySerialization => {
-            (Serializer::serialize(data, Some(json.len()))?, "!WA:2!")
-        }
-        _ => return Err("Unsupported StringVersion"),
-    };
-
-    let compressed = {
-        use flate2::{Compression, read::DeflateEncoder};
-        use std::io::prelude::*;
-
-        let mut result = Vec::new();
-        let mut deflater = DeflateEncoder::new(serialized.as_slice(), Compression::best());
-
-        deflater
-            .read_to_end(&mut result)
-            .map(|_| result)
-            .map_err(|_| "Compression error")
-    }?;
-
-    base64::encode_with_prefix(&compressed, prefix)
 }

.github/workflows/test.yml
@@ -15,7 +15,7 @@ jobs:
     - name: Install Rust
       uses: actions-rs/toolchain@v1
       with:
-        toolchain: 1.86.0
+        toolchain: 1.87.0
         profile: minimal
         override: true
         components: rustfmt, clippy

native/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "node-weakauras-parser"
-version = "3.2.5"
+version = "3.2.6"
 authors = ["Velithris"]
 edition = "2024"
 license = "MIT"
@@ -13,10 +13,15 @@ crate-type = ["cdylib"]
 lto = true # increases compile time, decreases size of a binary
 strip = true
 
+[features]
+default = ["gpl-dependencies"]
+gpl-dependencies = ["weakauras-codec/gpl-dependencies"]
+
 [dependencies]
 neon = { version = "1", features = ["napi-4"], default-features = false }
-flate2 = { version = "1", features = ["zlib-ng-compat"], default-features = false }
 serde_json = { version = "1", features = ["preserve_order"] }
-ryu = "1"
-itoa = "1"
-indexmap = "1"
+
+[dependencies.weakauras-codec]
+version = "0.1.0"
+default-features = false
+features = ["flate2-zlib-ng", "serde", "lua-value-indexmap"]

native/src/ace_serialize/deserialization/mod.rs
@@ -1,159 +1,0 @@
-use crate::macros::check_recursion;
-use serde_json::{Number, Value, map::Map};
-
-mod reader;
-use reader::StrReader;
-
-pub struct Deserializer<'s> {
-    remaining_depth: usize,
-    reader: StrReader<'s>,
-}
-
-impl<'s> Deserializer<'s> {
-    pub fn from_str(slice: &'s str) -> Self {
-        Self {
-            remaining_depth: 128,
-            reader: StrReader::new(slice),
-        }
-    }
-
-    /// Returns an array of deserialized values
-    #[allow(dead_code)]
-    pub fn deserialize(mut self) -> Result<Value, &'static str> {
-        self.reader.read_identifier().and_then(|v| {
-            if v == "^1" {
-                Ok(())
-            } else {
-                Err("Supplied data is not AceSerializer data (rev 1)")
-            }
-        })?;
-
-        let mut result = Vec::new();
-
-        while self.reader.peek_identifier().is_ok() {
-            if let Some(v) = self.deserialize_helper()? {
-                result.push(v);
-            }
-        }
-
-        Ok(Value::Array(result))
-    }
-
-    /// Returns the first deserialized value
-    #[allow(dead_code)]
-    pub fn deserialize_first(mut self) -> Result<Value, &'static str> {
-        self.reader.read_identifier().and_then(|v| {
-            if v == "^1" {
-                Ok(())
-            } else {
-                Err("Supplied data is not AceSerializer data (rev 1)")
-            }
-        })?;
-
-        let value = match self.deserialize_helper()? {
-            Some(v) => v,
-            _ => Value::Null,
-        };
-
-        Ok(value)
-    }
-
-    fn deserialize_helper(&mut self) -> Result<Option<Value>, &'static str> {
-        Ok(Some(match self.reader.read_identifier()? {
-            "^^" => return Ok(None),
-            "^Z" => Value::Null,
-            "^B" => Value::Bool(true),
-            "^b" => Value::Bool(false),
-            "^S" => Value::String(self.reader.parse_str()?.to_string()),
-            "^N" => self
-                .reader
-                .read_until_next()
-                .and_then(Self::deserialize_number)
-                .map(|v| match Number::from_f64(v) {
-                    Some(v) => Value::Number(v),
-                    None => Value::Null,
-                })?,
-            "^F" => {
-                let mantissa = self
-                    .reader
-                    .read_until_next()
-                    .and_then(|v| v.parse::<f64>().map_err(|_| "Failed to parse a number"))?;
-                let exponent = match self.reader.read_identifier()? {
-                    "^f" => self
-                        .reader
-                        .read_until_next()
-                        .and_then(|v| v.parse::<f64>().map_err(|_| "Failed to parse a number"))?,
-                    _ => return Err("Missing exponent"),
-                };
-
-                match Number::from_f64(mantissa * 2f64.powf(exponent)) {
-                    Some(v) => Value::Number(v),
-                    None => Value::Null,
-                }
-            }
-            "^T" => {
-                let mut keys = Vec::with_capacity(16);
-                let mut values = Vec::with_capacity(16);
-
-                loop {
-                    match self.reader.peek_identifier()? {
-                        "^t" => {
-                            let _ = self.reader.read_identifier();
-                            break;
-                        }
-                        _ => {
-                            check_recursion!(self, {
-                                let key = self.deserialize_helper()?.ok_or("Missing key")?;
-                                let value = match self.reader.peek_identifier()? {
-                                    "^t" => return Err("Unexpected end of a table"),
-                                    _ => self.deserialize_helper()?.ok_or("Missing value")?,
-                                };
-
-                                keys.push(key);
-                                values.push(value);
-                            });
-                        }
-                    }
-                }
-
-                debug_assert_eq!(keys.len(), values.len());
-                let is_array = keys.iter().zip(1usize..).all(|(el, i)| {
-                    if let Value::Number(el) = el {
-                        el.as_f64().map(|el| el == i as f64).unwrap_or(false)
-                    } else {
-                        false
-                    }
-                });
-
-                if is_array {
-                    Value::Array(values)
-                } else {
-                    let mut result = Map::with_capacity(keys.len());
-                    for (key, value) in keys.into_iter().zip(values.into_iter()) {
-                        let key = match key {
-                            Value::String(s) => Ok(s),
-                            Value::Number(n) => n
-                                .as_f64()
-                                .map(|v| v.to_string())
-                                .ok_or("Failed to parse a number"),
-                            Value::Bool(b) => Ok((if b { "true" } else { "false" }).into()),
-                            _ => Err("Unsupported type for an object key"),
-                        }?;
-
-                        result.insert(key, value);
-                    }
-                    Value::Object(result)
-                }
-            }
-            _ => return Err("Invalid identifier"),
-        }))
-    }
-
-    fn deserialize_number(data: &str) -> Result<f64, &'static str> {
-        match data {
-            "1.#INF" | "inf" => Ok(f64::INFINITY),
-            "-1.#INF" | "-inf" => Ok(f64::NEG_INFINITY),
-            v => v.parse().map_err(|_| "Failed to parse a number"),
-        }
-    }
-}

native/src/ace_serialize/deserialization/reader.rs
@@ -1,154 +1,0 @@
-// Based on SliceRead from serde_json
-// https://github.com/serde-rs/json
-// Licensed under either of Apache License, Version 2.0
-// or MIT license at your option (see LICENSES/serde_json).
-
-pub(crate) struct StrReader<'s> {
-    buffer: &'s [u8],
-    index: usize,
-    scratch: Vec<u8>,
-}
-
-#[allow(dead_code)]
-impl<'s> StrReader<'s> {
-    pub(crate) fn new(buffer: &'s str) -> Self {
-        Self {
-            buffer: buffer.as_bytes(),
-            index: 0,
-            scratch: Vec::new(),
-        }
-    }
-
-    #[inline]
-    fn next(&mut self) -> Option<u8> {
-        if self.index < self.buffer.len() {
-            let c = self.buffer[self.index];
-            self.index += 1;
-            Some(c)
-        } else {
-            None
-        }
-    }
-
-    #[inline]
-    fn peek(&self) -> Option<u8> {
-        if self.index < self.buffer.len() {
-            Some(self.buffer[self.index])
-        } else {
-            None
-        }
-    }
-
-    #[inline]
-    fn discard(&mut self) {
-        self.index += 1;
-    }
-
-    #[inline]
-    pub(crate) fn position(&self) -> usize {
-        self.index
-    }
-
-    pub(crate) fn read_identifier(&mut self) -> Result<&str, &'static str> {
-        if self.index + 1 < self.buffer.len() {
-            // Matching against the second byte to ensure
-            // we don't break up a multibyte character.
-            match (self.buffer[self.index], self.buffer[self.index + 1]) {
-                (b'^', 0x00..=0x79) => {
-                    let result = unsafe {
-                        std::str::from_utf8_unchecked(&self.buffer[self.index..self.index + 2])
-                    };
-                    self.index += 2;
-                    Ok(result)
-                }
-                _ => Err("Not an identifier"),
-            }
-        } else {
-            Err("Unexpected EOF")
-        }
-    }
-
-    pub(crate) fn peek_identifier(&self) -> Result<&str, &'static str> {
-        if self.index + 1 < self.buffer.len() {
-            // Matching against the second byte to ensure
-            // we don't break up a multibyte character.
-            match (self.buffer[self.index], self.buffer[self.index + 1]) {
-                (b'^', 0x00..=0x79) => Ok(unsafe {
-                    std::str::from_utf8_unchecked(&self.buffer[self.index..self.index + 2])
-                }),
-                _ => Err("Not an identifier"),
-            }
-        } else {
-            Err("Unexpected EOF")
-        }
-    }
-
-    pub(crate) fn read_until_next(&mut self) -> Result<&str, &'static str> {
-        let start = self.index;
-
-        loop {
-            match self.peek() {
-                None => return Err("Unexpected EOF"),
-                Some(b'^') => {
-                    // SAFETY: As long as `start` does not point at the middle
-                    // of a multibyte character, this should be safe.
-                    // Public API does not allow the reader to end up in such a state.
-                    return Ok(unsafe {
-                        std::str::from_utf8_unchecked(&self.buffer[start..self.index])
-                    });
-                }
-                _ => self.discard(),
-            }
-        }
-    }
-
-    pub(crate) fn parse_str(&mut self) -> Result<&str, &'static str> {
-        self.scratch.clear();
-
-        let mut copy_from = self.index;
-
-        loop {
-            match self.peek() {
-                None => return Err("Unexpected EOF"),
-                Some(b'^') => {
-                    if self.scratch.is_empty() {
-                        // SAFETY: As long as `copy_from` does not point at the middle
-                        // of a multibyte character, this should be safe.
-                        // Public API does not allow the reader to end up in such a state.
-                        return Ok(unsafe {
-                            std::str::from_utf8_unchecked(&self.buffer[copy_from..self.index])
-                        });
-                    } else {
-                        // SAFETY: None of the replaced bytes and their replacements
-                        // has the most significant bit set to 1.
-                        self.scratch
-                            .extend_from_slice(&self.buffer[copy_from..self.index]);
-                        return Ok(unsafe { std::str::from_utf8_unchecked(&self.scratch) });
-                    }
-                }
-                Some(b'~') => {
-                    self.scratch
-                        .extend_from_slice(&self.buffer[copy_from..self.index]);
-
-                    self.discard();
-
-                    let replacement = match self.peek() {
-                        Some(v @ 0x40..=0x5D) | Some(v @ 0x5F..=0x60) => v - 64,
-                        Some(0x7A) => 0x1E,
-                        Some(0x7B) => 0x7F,
-                        Some(0x7C) => 0x7E,
-                        Some(0x7D) => 0x5E,
-                        _ => return Err("Invalid escape character"),
-                        // _ => continue,
-                    };
-
-                    self.discard();
-                    self.scratch.push(replacement);
-
-                    copy_from = self.index;
-                }
-                _ => self.discard(),
-            }
-        }
-    }
-}

native/src/ace_serialize/mod.rs
@@ -1,6 +1,0 @@
-pub mod deserialization;
-#[allow(dead_code)]
-pub mod serialization;
-
-pub use deserialization::Deserializer;
-pub use serialization::Serializer;

native/src/ace_serialize/serialization/mod.rs
@@ -1,147 +1,0 @@
-use crate::macros::check_recursion;
-use serde_json::Value;
-
-fn f64_to_parts(v: f64) -> (u64, i16, i8) {
-    let bits = v.to_bits();
-    let sign: i8 = if bits >> 63 == 0 { 1 } else { -1 };
-    let mut exponent: i16 = ((bits >> 52) & 0x7ff) as i16;
-    let mantissa = if exponent == 0 {
-        (bits & 0xfffffffffffff) << 1
-    } else {
-        (bits & 0xfffffffffffff) | 0x10000000000000
-    };
-    exponent -= 1023 + 52;
-    (mantissa, exponent, sign)
-}
-
-fn write_integer_to_a_string<I>(string: &mut String, value: I)
-where
-    I: itoa::Integer,
-{
-    let mut buffer = itoa::Buffer::new();
-    let serialized = buffer.format(value);
-    string.push_str(serialized)
-}
-
-pub struct Serializer {
-    remaining_depth: usize,
-    result: String,
-}
-
-impl Serializer {
-    pub fn serialize(
-        value: &Value,
-        approximate_len: Option<usize>,
-    ) -> Result<String, &'static str> {
-        let mut serializer = Self {
-            remaining_depth: 128,
-            result: String::with_capacity(approximate_len.unwrap_or(1024)),
-        };
-
-        serializer.result.push_str("^1");
-        serializer.serialize_helper(value)?;
-        serializer.result.push_str("^^");
-
-        Ok(serializer.result)
-    }
-
-    fn serialize_helper(&mut self, value: &Value) -> Result<(), &'static str> {
-        match *value {
-            Value::Null => self.result.push_str("^Z"),
-            Value::Bool(b) => self.result.push_str(if b { "^B" } else { "^b" }),
-            Value::String(ref s) => {
-                self.result.push_str("^S");
-                self.serialize_string(s)
-            }
-            Value::Number(ref n) => n
-                .as_f64()
-                .ok_or("Failed to parse a number")
-                .and_then(|n| self.serialize_number(n))?,
-            Value::Array(ref vec) => {
-                self.result.reserve(vec.len() * 6 + 4);
-
-                self.result.push_str("^T");
-                for (i, v) in vec.iter().enumerate() {
-                    check_recursion!(self, {
-                        self.result.push_str("^N");
-                        write_integer_to_a_string(&mut self.result, i + 1);
-                        self.serialize_helper(v)?;
-                    });
-                }
-                self.result.push_str("^t");
-            }
-            Value::Object(ref m) => {
-                self.result.reserve(m.len() * 6 + 4);
-
-                self.result.push_str("^T");
-                for (key, value) in m.iter() {
-                    check_recursion!(self, {
-                        self.result.push_str(if key.parse::<i32>().is_ok() {
-                            "^N"
-                        } else {
-                            "^S"
-                        });
-                        self.result.push_str(key);
-                        self.serialize_helper(value)?;
-                    });
-                }
-                self.result.push_str("^t");
-            }
-        }
-
-        Ok(())
-    }
-
-    fn serialize_number(&mut self, value: f64) -> Result<(), &'static str> {
-        if value.is_nan() {
-            return Err("AceSerializer does not support NaNs");
-        } else if !value.is_finite() {
-            self.result.push_str("^N");
-            self.result
-                .push_str(if value > 0.0 { "1.#INF" } else { "-1.#INF" })
-        } else {
-            let mut buffer = ryu::Buffer::new();
-            let str_value = buffer.format_finite(value);
-
-            if str_value.parse::<f64>().unwrap() == value {
-                self.result.reserve(str_value.len() + 2);
-                self.result.push_str("^N");
-                self.result.push_str(str_value);
-            } else {
-                let (mantissa, exponent, sign) = f64_to_parts(value);
-                self.result.push_str("^F");
-                if sign < 0 {
-                    self.result.push('-');
-                }
-                write_integer_to_a_string(&mut self.result, mantissa);
-                self.result.push_str("^f");
-                write_integer_to_a_string(&mut self.result, exponent);
-            }
-        }
-
-        Ok(())
-    }
-
-    fn serialize_string(&mut self, value: &str) {
-        self.result.reserve(value.len());
-
-        let mut copy_from = 0;
-        for (i, byte) in value.bytes().enumerate() {
-            let replacement = match byte {
-                v @ 0x00..=0x1D | v @ 0x1F..=0x20 => v + 64,
-                0x1E => 0x7A,
-                0x5E => 0x7D,
-                0x7E => 0x7C,
-                0x7F => 0x7B,
-                _ => continue,
-            };
-
-            self.result.push_str(&value[copy_from..i]);
-            self.result.push('~');
-            self.result.push(replacement as char);
-            copy_from = i + 1;
-        }
-
-        self.result.push_str(&value[copy_from..]);
-    }
-}

native/src/asynchronous.rs
@@ -1,32 +1,35 @@
 use neon::prelude::*;
 
 use super::common;
+use weakauras_codec::{decode, encode};
 
 pub fn decode_weakaura(mut cx: FunctionContext) -> JsResult<JsPromise> {
     let src = cx.argument::<JsString>(0)?.value(&mut cx);
     let max_size = common::parse_max_size(cx.argument_opt(1), &mut cx)?;
 
     let promise = cx
-        .task(move || common::decode_weakaura(&src, max_size))
+        .task(move || decode(src.as_bytes().trim_ascii_end(), max_size))
         .promise(|mut cx, result| {
             result
+                .or_else(|e| cx.throw_error(e.to_string()))
+                .and_then(|v| serde_json::to_string(&v).or_else(|e| cx.throw_error(e.to_string())))
                 .map(|json| cx.string(json))
-                .or_else(|e| cx.throw_error(e))
         });
 
     Ok(promise)
 }
 
 pub fn encode_weakaura(mut cx: FunctionContext) -> JsResult<JsPromise> {
-    let src = cx.argument::<JsString>(0)?.value(&mut cx);
+    let json = cx.argument::<JsString>(0)?.value(&mut cx);
+    let src = serde_json::from_str(&json).or_else(|e| cx.throw_error(e.to_string()))?;
     let string_version = common::parse_string_version(cx.argument_opt(1), &mut cx)?;
 
     let promise = cx
-        .task(move || common::encode_weakaura(&src, string_version))
+        .task(move || encode(&src, string_version))
         .promise(|mut cx, result| {
             result
                 .map(|serialized| cx.string(serialized))
-                .or_else(|e| cx.throw_error(e))
+                .or_else(|e| cx.throw_error(e.to_string()))
         });
 
     Ok(promise)

commit 4166541cd1e3008ae8d83d7a1823b6ad5d5ef4e0

Fix `+Infinity` being improperly handled when used as the memory usage limit
The regression was introduced in 6377c13e7a39872a7443cff073eb609efeeae47c,
when the core logic was moved into a separate library.

native/src/common.rs
@@ -13,7 +13,7 @@ pub fn parse_max_size<'a>(
                 v.downcast_or_throw::<JsNumber, _>(cx).and_then(|v| {
                     let v = v.value(cx);
                     if v == f64::INFINITY {
-                        Ok(None)
+                        Ok(Some(usize::MAX))
                     } else if v.is_finite() && v >= 0.0 {
                         Ok(Some(v.trunc() as usize))
                     } else {